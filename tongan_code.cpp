9a31b60aa769588653141af9dad27035c82e6978
jiporCK <srengchipor99@gmail.com>
2026-03-10 14:38:10 +0700

    modify code

--- tongan_code.cpp
+++ tongan_code.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-using namespace std;
+// using namespace std;
 
 int main(){
-    cout << "CVD 10 10";
-    cout << "CVD 10 10";
-    cout << "CVD 10 10";
-    cout << "CVD 10 10";
-    cout << "CVD 10 10";
+    std::cout << "CVD 10 10";
+    std::cout << "CVD 10 10";
+    std::cout << "41 41 cr ke cr yg";
+    std::cout << "CVD 10 10";
+    std::cout << "41 41 cr ke cr yg";
+    std::cout << "CVD 10 10";
 
-
-
-    
-    cout << "41 41 cr ke cr yg";
     return 0;
 }
